834342c7621d2147addd35b8fdf7985faad519f5
Shadowx9920 <youssefoutahar1@gmail.com>
2023-07-31 14:55:04 +0100

    added ts logic to nonAffectedCollabs

--- src/app/components/collab-non-affected-manager/collab-non-affected-manager.component.ts
+++ src/app/components/collab-non-affected-manager/collab-non-affected-manager.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Collaborateur } from 'src/app/classes/collaborateur';
+import { CollaborateurService } from 'src/app/services/collaborateur.service';
 
 @Component({
   selector: 'app-collab-non-affected-manager',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollabNonAffectedManagerComponent implements OnInit {
 
-  constructor() { }
+  nonAffectedCollabs !: Collaborateur[];
+  constructor(private collaborateurService:CollaborateurService,private router:Router) { }
 
   ngOnInit() {
   }
 
+  getNonAffectedCollabs(){
+    this.collaborateurService.getNonAffectedCollabs().subscribe(data=>{
+      this.nonAffectedCollabs=data;
+      console.log(data);  },
+      error => console.log(error));
+  }
 }
